81766e5c23870006ff872a9eab11f478e812b05d
Elkin Alexis Llanos Cardenas <llanoscardenase@gmail.com>
2024-06-10 15:04:28 -0500

    first commit

---
+++ src/app/game.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { IPiece } from './piece/piece.component';
+import { COLS, ROWS, POINTS } from './constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  constructor() { }
+
+  // Comprueba si la nueva posición de una pieza es correcta
+  // Se invoca a las funciones correspondientes para determinar
+  // Si la nueva posición de la pieza se saldría del tablero u
+  // ocuparía alguna posición ya ocupada por otra pieza.
+  valid(p: IPiece, board: number[][]): boolean {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return (
+          this.isEmpty(value) ||
+          (this.insideWalls(x) &&
+            this.aboveFloor(y) &&
+            this.notOccupied(board, x, y))
+        );
+      });
+    });
+  }
+
+  // Comprueba si la posición está vacía (0 -> vacía)
+  isEmpty(value: number): boolean {
+    return value === 0;
+  }
+
+  // Comprueba si la posición está dentro de la pantalla
+  insideWalls(x: number): boolean {
+    return x >= 0 && x < COLS;
+  }
+
+  // Comprueba si la posición está por debajo del suelo
+  aboveFloor(y: number): boolean {
+    return y <= ROWS;
+  }
+
+  // Comprueba si la posición no está ocupada
+  notOccupied(board: number[][], x: number, y: number): boolean {
+    return board[y] && board[y][x] === 0;
+  }
+
+  // Rota una pieza alrededor de su centro de rotación, 
+  // la coordenada central de la matriz de cada forma.
+  rotate(piece: IPiece): IPiece {
+    let p: IPiece = JSON.parse(JSON.stringify(piece));
+    for (let y = 0; y < p.shape.length; ++y) {
+      for (let x = 0; x < y; ++x) {
+        [p.shape[x][y], p.shape[y][x]] = [p.shape[y][x], p.shape[x][y]];
+      }
+    }
+    p.shape.forEach(row => row.reverse());
+    return p;
+  }
+
+  // Calcula los puntos obtenidos al limpiar las líneas
+  getLinesClearedPoints(lines: number, level: number): number {
+    const lineClearPoints =
+      lines === 1
+        ? POINTS.SINGLE
+        : lines === 2
+        ? POINTS.DOUBLE
+        : lines === 3
+        ? POINTS.TRIPLE
+        : lines === 4
+        ? POINTS.TETRIS
+        : 0;
+
+    return (level + 1) * lineClearPoints;
+  }
+}
---
+++ src/app/piece/piece.component.ts
@@ -0,0 +1,71 @@
+// Se importan las constantes del juego
+import { COLORS, SHAPES } from '../constants';
+
+// Esta interfaz representa las características de una pieza
+// del tablero (coordenadas, color y tipo de forma)
+// Las formas son del tipo I, J, L, O, S, T y Z
+export interface IPiece {
+  x: number;
+  y: number;
+  color: string;
+  shape: number[][];
+}
+
+export class Piece implements IPiece {
+  x: number;
+  y: number;
+  color: string;
+  shape: number[][];
+
+  constructor(private ctx: CanvasRenderingContext2D) {
+    this.spawn();
+  }
+
+  // Esta función genera una pieza aleatoria y la configura
+  spawn() {
+    const typeId = this.randomizeTetrominoType(COLORS.length - 1);
+    this.shape = SHAPES[typeId];
+    this.color = COLORS[typeId];
+    this.x = typeId === 4 ? 4 : 3;
+    this.y = 0;
+  }
+
+  // Esta función mueve la pieza
+  move(p: IPiece) {
+    this.x = p.x;
+    this.y = p.y;
+    this.shape = p.shape;
+  }
+
+  // Dibuja la pieza en el canvas
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          // this.x & this.y = posición en el tablero
+          // x & y son las posiciones de la pieza
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
+
+  // Pinta la pieza siguiente
+  drawNext(ctxNext: CanvasRenderingContext2D) {
+    ctxNext.clearRect(0, 0, ctxNext.canvas.width, ctxNext.canvas.height);
+    ctxNext.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          ctxNext.fillRect(x, y, 1, 1);
+        }
+      });
+    });
+  }
+
+  // Genera una forma aleatoria de entre todas las posibles
+  randomizeTetrominoType(noOfTypes: number): number {
+    return Math.floor(Math.random() * noOfTypes + 1);
+  }
+}
